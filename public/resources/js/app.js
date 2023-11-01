4e87df30234a60c845b976772f721ae7fec8be69
DhyasR <wramadhian@student.ciputra.ac.id>
2023-11-01 21:40:06 +0700

    new update for navbar

---
+++ public/resources/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const profileButton = document.getElementById('profileButton');
+    const profileMenu = document.getElementById('profileMenu');
+
+    profileButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        profileMenu.classList.toggle('visible'); // Tambahkan atau hapus kelas "visible"
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!profileMenu.contains(event.target) && event.target !== profileButton) {
+            profileMenu.classList.remove(
+                'visible'); // Hapus kelas "visible" jika mengklik di luar menu
+        }
+    });
+});
