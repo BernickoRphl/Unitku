beaca5d455d7051586ab444a1677e02a61a708c2
DhyasR <wramadhian@student.ciputra.ac.id>
2023-11-04 08:45:19 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -16,41 +16,51 @@
         <section class="footer">
 
             <div class="box-container">
+
                 <div class="box">
+
                     <h3>quick links</h3>
                     <a href="">home</a>
                     <a href="">products</a>
                     <a href="">team</a>
                     <a href="">about</a>
                     <a href="">contacts</a>
+
                 </div>
 
                 <div class="box">
+
                     <h3>extra links</h3>
                     <a href="">extra1</a>
                     <a href="">extra2</a>
                     <a href="">extra3</a>
                     <a href="">extra4</a>
                     <a href="">extra5</a>
+
                 </div>
 
                 <div class="box">
+
                     <h3>locations</h3>
                     <a href="">Indonesia</a>
                     <a href="">Surabaya</a>
                     <a href="">Location1</a>
                     <a href="">Location2</a>
                     <a href="">Location3</a>
+
                 </div>
 
                 <div class="box">
+
                     <h3>contact info</h3>
                     <a href="">Line</a>
                     <a href="">Number</a>
                     <a href="">Email</a>
                     <a href="">Instagram</a>
                     <a href="">contacts</a>
+
                 </div>
+
             </div>
 
             <div class="credit">
