216915791abe199ebffb905b15016da04b7b36b5
DhyasR <wramadhian@student.ciputra.ac.id>
2023-11-11 09:05:51 +0700

    update code

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TeamSeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class TeamSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('teams')->insert(
+            [
+                'name' => 'DANIEL KARIYADI',
+                'position' => 'CEO',
+                'team_image' => 'daniel.png'
+            ]
+        );
+
+        DB::table('teams')->insert(
+            [
+                'name' => 'BRYANNA JERSEY',
+                'position' => 'CTO',
+                'team_image' => 'bry.png'
+            ]
+        );
+
+        DB::table('teams')->insert(
+            [
+                'name' => 'ALDRICH JEREMIAH',
+                'position' => 'CFO',
+                'team_image' => 'jj.png'
+            ]
+        );
     }
 }
