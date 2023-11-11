15268de46d063a971123bb7d1905e60e9776abe2
BernickoRphl <braphael@student.ciputra.ac.id>
2023-11-11 15:36:02 +0700

    benerin view lagi

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -68,22 +68,62 @@
         </div>
 
     </section>
-    <h1 class="heading">Our <span>Team</span></h1>
 
-    @foreach ($teams as $teamMember)
-    <section class="team" id="team{{ $teamMember->id }}">
+    {{-- <section class="team" id="team">
+        <h1 class="heading">Our <span>Team</span></h1>
+
         <div class="box-container">
+
             <div class="box">
-                <span class="classification">{{ $teamMember->position }}</span>
+                <span class="classification">CEO</span>
                 <div class="image">
-                    <img src="/resources/images/{{ $teamMember->team_image }}" alt="" />
+                    <img src="/resources/images/daniel.png" alt="" />
                 </div>
                 <div class="content">
-                    <h3>{{ $teamMember->name }}</h3>
-                    <div class="price">{{ $teamMember->desc }}</div>
+                    <h3>DANIEL KARIYADI</h3>
+                    <div class="price"> I'm part of JCI and several experiences on clothing industry.</div>
                 </div>
             </div>
-        </div>
-    </section>
-@endforeach
-@endsection
+
+            <div class="box">
+                <span class="classification">CTO</span>
+                <div class="image">
+                    <img src="/resources/images/bry.png" alt="" />
+                </div>
+                <div class="content">
+                    <h3>BRYANNA JERSEY</h3>
+                    <div class="price"> I have several exoeriences on branding products.</div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="classification">CFO</span>
+                <div class="image">
+                    <img src="/resources/images/jj.png" alt="" />
+                </div>
+                <div class="content">
+                    <h3>ALDRICH JEREMIAH</h3>
+                    <div class="price"> I'm good at managing financial.</div>
+                </div>
+            </div>
+    </section> --}}
+
+    <h1 class="heading">Our <span>Team</span></h1>
+
+    <div class="box-container">
+        @foreach ($teams as $teamMember)
+            <section class="team" id="team{{ $teamMember->id }}">
+                <div class="box">
+                    <span class="classification">{{ $teamMember->position }}</span>
+                    <div class="image">
+                        <img src="/resources/images/{{ $teamMember->team_image }}" alt="" />
+                    </div>
+                    <div class="content">
+                        <h3>{{ $teamMember->name }}</h3>
+                        <div class="price">{{ $teamMember->desc }}</div>
+                    </div>
+                </div>
+            </section>
+        @endforeach
+    </div>
+
