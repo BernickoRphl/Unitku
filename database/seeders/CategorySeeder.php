5136c98883d84ddee00be65743d7416378ca6ad8
BernickoRphl <120437917+BernickoRphl@users.noreply.github.com>
2023-12-14 15:40:10 +0700

    migration

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert(
+            [
+                'name' => 'Full Set',
+                'description' => 'Spine + Sleves + Sheets',
+            ]
+        );
+        DB::table('categories')->insert(
+            [
+                'name' => 'Sleeves',
+                'description' => 'Sleves Only',
+            ]
+        );DB::table('categories')->insert(
+            [
+                'name' => 'Sheets',
+                'description' => 'Sheets Only',
+            ]
+        );
     }
 }
