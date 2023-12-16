b96bae544037ed234905d1595aff9bae7a088494
DhyasR <wramadhian@student.ciputra.ac.id>
2023-12-16 23:02:18 +0700

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('link')
     <link rel="stylesheet" href="resources/css/home.css">
-    <script src="resources/js/app.js"></script>
 @endsection
 
 @section('content')
@@ -11,7 +10,8 @@
         <div class="content">
             <h3>INTRIC</h3>
             <span>What is it?</span>
-            <p class="text-justify">Intric is a detachable clothing company created in late 2022 and based in Indonesia. it focuses on making
+            <p class="text-justify">Intric is a detachable clothing company created in late 2022 and based in Indonesia. it
+                focuses on making
                 simple but intricately designed functional clothing in the form of a shirt with detachable parts. Intric
                 aims to create clothing that is flexible towards the customer's taste by allowing customisation to the
                 detachable areas. The concept has been around for quite some time, but Intric is the first one to do it
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -85,7 +85,47 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
+                        <div class="relative ml-3">
+
+                            <div>
+
+                                <button type="button" id="profileButton"
+                                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img class="h-10 w-10 rounded-full"
+                                        src="https://static.vecteezy.com/system/resources/previews/013/331/127/original/account-avatar-dark-mode-glyph-ui-icon-personal-page-of-site-user-user-interface-design-white-silhouette-symbol-on-black-space-solid-pictogram-for-web-mobile-isolated-illustration-vector.jpg"
+                                        alt="">
+
+                                </button>
+
+                            </div>
+
+                            <div id="profileMenu"
+                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                                tabindex="-1">
+
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                    tabindex="-1" id="user-menu-item-1">Settings</a>
+
+                                <a class="dropdown-item text-black block px-4 py-2 text-sm text-gray-700"
+                                    href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+
+                            </div>
+
+                        </div>
+                        {{-- <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
@@ -102,7 +142,7 @@
                                     @csrf
                                 </form>
                             </div>
-                        </li>
+                        </li> --}}
                     @endguest
 
                 </ul>
