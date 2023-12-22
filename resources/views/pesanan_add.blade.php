65729d66487543f8d7866c5cb708881cc86c6bf8
BernickoRphl <120437917+BernickoRphl@users.noreply.github.com>
2023-12-22 16:58:11 +0700

    jquery

--- resources/views/pesanan_add.blade.php
+++ resources/views/pesanan_add.blade.php
@@ -6,18 +6,30 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 @endsection
 
 @section('content')
+
+<style>
+    #products {
+        width: 50%;
+    }
+</style>
+
     <div class="mt-40 mb-40">
         <form method="POST" action="{{ route('pesanan.add') }}">
             @csrf
 
             <div class="row mb-3">
-                <label for="tanggal_pemesanan" class="col-md-4 col-form-label text-md-end">{{ __('Tanggal Pemesanan') }}</label>
+                <label for="tanggal_pemesanan"
+                    class="col-md-4 col-form-label text-md-end">{{ __('Tanggal Pemesanan') }}</label>
                 <div class="col-md-6">
-                    <input id="tanggal_pemesanan" type="date" class="form-control" name="tanggal_pemesanan" value="{{ $currentDate }}" required>
+                    <input id="tanggal_pemesanan" type="date" class="form-control" name="tanggal_pemesanan"
+                        value="{{ $currentDate }}" required>
                 </div>
             </div>
 
@@ -38,15 +50,42 @@
             <div class="row mb-3">
                 <label for="products" class="col-md-4 col-form-label text-md-end">{{ __('Products') }}</label>
                 <div class="col-md-6">
-                    <!-- Add a dropdown or other input for selecting products -->
-                    <!-- Example: -->
+                    <!-- Use the Select2 library for an enhanced dropdown -->
                     <select name="products[]" id="products" multiple required>
                         @foreach ($product as $pro)
-                            <option value="{{ $pro->id }}">{{ $pro->product_name }}</option>
+                            <option value="{{ $pro->id }}" data-image="{{ asset('storage/' . $pro->product_image) }}">
+                                {{ $pro->product_name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
             </div>
+
+            <script>
+                // JQUERY
+                $(document).ready(function() {
+                    $('#products').select2({
+                        templateResult: formatProduct,
+                        escapeMarkup: function(m) {
+                            return m;
+                        }
+                    });
+                });
+
+                // Custom function to format the dropdown option
+                function formatProduct(product) {
+                    if (!product.id) {
+                        return product.text;
+                    }
+                    var $product = $(
+                        '<span><img src="' + $(product.element).data('image') + '" class="w-10 h-auto" /> ' + product.text +
+                        '</span>'
+                    );
+                    return $product;
+                }
+            </script>
+
+
 
             <div class="row mb-3">
                 <label for="jumlah" class="col-md-4 col-form-label text-md-end">{{ __('Jumlah') }}</label>
