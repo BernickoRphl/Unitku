e9dc10e4dc2ff6597dab4e715b2f55fccf85fe91
DhyasR <wramadhian@student.ciputra.ac.id>
2023-12-22 20:12:16 +0700

    new

--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -5,15 +5,36 @@
 @endsection --}}
 
 @section('content')
-    <div class="flex justify-center items-center h-screen">
-        <div class="text-center p-5 bg-primary text-white rounded">
-            <h1 class="text-3xl mb-4">{{ $product['product_name'] }}</h1>
-            <p class="mb-2">Description: {{ $product['product_desc'] }}</p>
-            <p class="mb-2">Image:</p>
-            <img src="{{ asset('storage/' . $product->product_image) }}" alt="{{ $product->product_name }}"
-                class="w-40 h-auto mb-2 mx-auto">
-            <p class="mb-2">Price: {{ $product['price'] }}</p>
-            <p class="mb-2">Color: {{ $product['color'] }}</p>
+    <div class="flex justify-center items-center mt-60 mb-40">
+
+        <div class="flex flex-col gap-5 items-center">
+
+            <h1 class="text-8xl font-semi-bold">{{ $product['product_name'] }}</h1>
+
+            <p class="text-2xl">Description: {{ $product['product_desc'] }}</p>
+
+            <table>
+
+                <td class="">
+
+                    <p class="text-2xl mr-2">Image:</p>
+
+                </td>
+
+                <td>
+
+                    <img src="{{ asset('storage/' . $product->product_image) }}" alt="{{ $product->product_name }}"
+                        class="w-40 h-auto mb-2 mx-auto ml-2">
+
+                </td>
+
+            </table>
+
+            <p class="text-2xl">Price: {{ $product['price'] }}</p>
+
+            <p class="text-2xl">Color: {{ $product['color'] }}</p>
+
         </div>
+
     </div>
 @endsection
