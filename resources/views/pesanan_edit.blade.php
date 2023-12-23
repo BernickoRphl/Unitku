bd37b14d0b48a25e78f6dca7313f58fb9b28cdfd
DhyasR <wramadhian@student.ciputra.ac.id>
2023-12-23 19:16:27 +0700

    new

--- resources/views/pesanan_edit.blade.php
+++ resources/views/pesanan_edit.blade.php
@@ -1,97 +1,97 @@
 @extends('layouts.template')
 
 @section('link')
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>{{ config('app.name', 'Laravel') }}</title>
+<link rel="dns-prefetch" href="//fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 @endsection
 
 @section('content')
-    <div class="mt-40 mb-40">
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('pesanan.update', $pesananEdit) }}" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-
-            <div class="row mb-3">
-                <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
-                <div class="col-md-6">
-                    <input id="address" type="text" class="form-control @error('name') is-invalid @enderror"
-                        name="address" value="{{ $pesananEdit->address }}" required autocomplete="address"
-                        autofocus>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-
-                <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
-
-                <div class="col-md-6">
-
-                    <input id="description" type="text" class="form-control" name="description"
-                        value="{{ $pesananEdit->description }}" required autocomplete="description">
-
-                </div>
-
-            </div>
-
-            <div class="row mb-3">
-
-                <label for="jumlah" class="col-md-4 col-form-label text-md-end">{{ __('Jumlah') }}</label>
-
-                <div class="col-md-6">
-
-                    <input id="jumlah" type="text" class="form-control" name="jumlah"
-                        value="{{ $pesananEdit->jumlah }}" required autocomplete="jumlah">
-
-                </div>
-
-            </div>
-
-            <div class="row mb-3">
-
-                <label for="category" class="col-md-4 col-form-label text-md-end">{{ __('Status') }}</label>
-
-                <div class="col-md-6">
-
-                    <select name="status" id="status" required>
-
-                        @foreach ($status as $stats)
-                            @if (old('$stats->id', $pesananEdit->stats) === $stats->id)
-                                <option value="{{ $stats->id }}" selected>{{ $stats->name }}</option>
-                            @else
-                                <option value="{{ $stats->id }}">{{ $stats->name }}</option>
-                            @endif
-                        @endforeach
-
-                    </select>
-
-                </div>
-
-            </div>
-
-            <div class="row mb-0">
-                <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary text-black">
-                        {{ __('Update Pesanan') }}
-                    </button>
-                </div>
-            </div>
-        </form>
+<div class="mt-40 mb-40">
+    @if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+    @endif
 
-    <script>
-        // Your existing script remains unchanged
-    </script>
+    <form method="POST" action="{{ route('pesanan.update', $pesananEdit) }}" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+
+        <div class="row mb-3">
+            <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
+            <div class="col-md-6">
+                <input id="address" type="text" class="form-control @error('name') is-invalid @enderror" name="address"
+                    value="{{ $pesananEdit->address }}" readonly autocomplete="address" autofocus>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+
+            <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
+
+            <div class="col-md-6">
+
+                <input id="description" type="text" class="form-control" name="description"
+                    value="{{ $pesananEdit->description }}" readonly autocomplete="description">
+
+            </div>
+
+        </div>
+
+        <div class="row mb-3">
+
+            <label for="jumlah" class="col-md-4 col-form-label text-md-end">{{ __('Jumlah') }}</label>
+
+            <div class="col-md-6">
+
+                <input id="jumlah" type="text" class="form-control" name="jumlah" value="{{ $pesananEdit->jumlah }}"
+                    readonly autocomplete="jumlah">
+
+            </div>
+
+        </div>
+
+        <div class="row mb-3">
+
+            <label for="category" class="col-md-4 col-form-label text-md-end">{{ __('Status') }}</label>
+
+            <div class="col-md-6">
+
+                <select name="status" id="status" required>
+
+                    @foreach ($status as $stats)
+                    @if (old('$stats->id', $pesananEdit->stats) === $stats->id)
+                    <option value="{{ $stats->id }}" selected>{{ $stats->name }}</option>
+                    @else
+                    <option value="{{ $stats->id }}">{{ $stats->name }}</option>
+                    @endif
+                    @endforeach
+
+                </select>
+
+            </div>
+
+        </div>
+
+        <div class="row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary text-black">
+                    {{ __('Update Pesanan') }}
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script>
+    // Your existing script remains unchanged
+
+</script>
 @endsection
