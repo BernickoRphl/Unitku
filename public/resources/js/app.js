2609a1be7b9345ba399bdf7af9ae4e421e9b302b
BernickoRphl <120437917+BernickoRphl@users.noreply.github.com>
2024-01-05 17:28:08 +0700

    new

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const profileButton = document.getElementById('profileButton');
+    const profileMenu = document.getElementById('profileMenu');
     const mobileMenuButton = document.querySelector('#mobile-menu-button');
     const mobileMenu = document.querySelector('#mobile-menu');
+
+    profileButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        profileMenu.classList.toggle('visible'); // Tambahkan atau hapus kelas "visible"
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!profileMenu.contains(event.target) && event.target !== profileButton) {
+            profileMenu.classList.remove(
+                'visible'); // Hapus kelas "visible" jika mengklik di luar menu
+        }
+    });
+
+    mobileMenuButton.addEventListener('click', function () {
+        mobileMenu.classList.toggle('hidden');
+    });
 
     // Sembunyikan menu pada awalnya
     if (mobileMenu) {
@@ -14,4 +32,9 @@ document.addEventListener('DOMContentLoaded', function () {
             mobileMenu.classList.toggle('hidden');
         });
     }
+    g
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     var profileMenu = document.getElementById('profileMenu');
+    //     profileMenu.classList.remove('hidden'); // Menghapus kelas hidden agar dropdown selalu terlihat
+    // });
 });
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,13 +22,18 @@
 
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
 
-                    <button type="button" id="mobile-menu-button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                    <button type="button" id="mobile-menu-button"
+                        class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        aria-controls="mobile-menu" aria-expanded="false">
                         <span class="absolute -inset-0.5"></span>
                         <span class="sr-only">Open main menu</span>
-                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
-                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
@@ -61,9 +66,10 @@
                                         class="{{ request()->is('pesanan/pesanan_index') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-xl font-medium">Order</a>
                                 @endif
                                 @if (Auth::user()->isSuperadmin())
-                                <a href="{{ route('admin.list') }}"
-                                    class="{{ request()->is('list_admin') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-xl font-medium">List Admin</a>
-                            @endif
+                                    <a href="{{ route('admin.list') }}"
+                                        class="{{ request()->is('list_admin') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-xl font-medium">List
+                                        Admin</a>
+                                @endif
                                 @if (Auth::user()->isAdmin())
                                     <a href="{{ route('product.list') }}"
                                         class="{{ request()->is('product/product_list') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-xl font-medium">List
@@ -101,10 +107,8 @@
 
                                 <button type="button" id="profileButton"
                                     class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-
+                                    aria-expanded="false" aria-haspopup="true">
                                     <h2 class="block px-4 py-2 text-md text-gray-200">{{ Auth::user()->name }}</h2>
-
                                     <span class="absolute -inset-1.5"></span>
                                     <span class="sr-only">Open user menu</span>
                                     <img class="h-10 w-10 rounded-full"
@@ -116,28 +120,21 @@
                             </div>
 
                             <div id="profileMenu"
-                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
                                 role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                 tabindex="-1">
-
+                                <!-- Konten dropdown -->
                                 <h2 class="block px-4 py-2 text-md text-black">{{ Auth::user()->name }}</h2>
-
-                                {{-- <a href="#" class="block px-4 py-2 text-sm text-gray-500 hover:text-gray-800"
-                                    role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a> --}}
-
                                 <a class="dropdown-item text-black block px-4 py-2 text-sm text-gray-500 hover:text-gray-800"
                                     href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
+                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
-
                             </div>
-
                         </div>
                     @endguest
 
@@ -255,18 +252,6 @@
     </footer>
 
     <script src="{{ asset('resources/js/app.js') }}" defer></script>
-
-    {{-- <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const mobileMenuButton = document.getElementById('mobile-menu-button');
-            const mobileMenu = document.getElementById('mobile-menu');
-
-            mobileMenuButton.addEventListener('click', function () {
-                mobileMenu.classList.toggle('hidden');
-            });
-        });
-    </script> --}}
-
 </body>
 
 </html>
